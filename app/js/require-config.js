6b9c5d21b1ea700f4f082bd94ffb1d6172c86e93
Elliot Smith <elliot.smith@intel.com>
2013-04-05 14:31:15 +0100

    Remove a couple of global variables

--- app/js/require-config.js
+++ app/js/require-config.js
@@ -12,12 +12,14 @@ require.config({
   }
 });
 
-// this is here as the r.js optimiser uses the first require.config
-// call in the file to configure its build (i.e. to minify and concat
-// all the JS files used by the app via require); the argument passed
-// to that first call must be valid JSON, and the below config is not
-var urlArgs = (document.location.href.match(/nocache/) ?
-               'bust=' + (new Date()).getTime() :
-               '');
+(function () {
+  // this is here as the r.js optimiser uses the first require.config
+  // call in the file to configure its build (i.e. to minify and concat
+  // all the JS files used by the app via require); the argument passed
+  // to that first call must be valid JSON, and the below config is not
+  var urlArgs = (document.location.href.match(/nocache/) ?
+                 'bust=' + (new Date()).getTime() :
+                 '');
 
-require.config({urlArgs: urlArgs});
+  require.config({urlArgs: urlArgs});
+})();
