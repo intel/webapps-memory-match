4f5e099226d6a9e492ff57eb99f0452bcffb17ef
Elliot Smith <elliot.smith@intel.com>
2013-04-15 11:04:32 +0100

    Add missing copyright headers on new JS files

--- app/js/ui.js
+++ app/js/ui.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2012, Intel Corporation.
+ *
+ * This program is licensed under the terms and conditions of the
+ * Apache License, version 2.0.  The full text of the Apache License is at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ */
+
 // links the Game object to the UI and directly
 // sets up event handlers on the DOM
 define(['game', 'jqmobi', 'domReady!'], function (Game, $) {
